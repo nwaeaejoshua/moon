c547ecefb1f80dc98779bba8a9ff8b53bef2319a
joshua <joshua>
2024-10-16 01:32:11 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let message = "hello"
+console.log(message)
+let add = "man"
+console.log(add)
+
+
+let name = "john";
+console.log(name)
+let firstname = "name";
+console.log(firstname)
+
+
+console.log(1)
+console.log("name")
+console.log(name)
+
+let food = "first";
+let time = "school";
+console.log (food +" "+ time)
+// +
+console.log (2 + 2)
+
+const elev = "eleven";
+const newnum ="twelve";
+console.Log (elev +" "+ newnum)
+// var
+let three = 3;
+let six = 6;
+console.log (three * six)
